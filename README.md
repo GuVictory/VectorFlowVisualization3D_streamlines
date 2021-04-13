a8a44610ecb1d8d5be956b987bcbc3336276d899
guvictory <gubanova.vi10@gmail.com>
2021-04-13 15:13:50 +0300

    feat: README fixed and images added

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# VectorFlowVisualization3D_streamlines
+# Построение векторного поля на неструктурированной сетке при помощи линий тока
+
+## Идея алгоритма
+Для расчета значения векторного поля в каждой точке линии тока используется трилинейная интерполяция.
+Для определения положения следующей точки линии тока используется алгоритм Рунге-Кутты 4-го порядка.
+
+## Инструменты
+* CMake - для сборки проекта
+* VTK@9 - для визуализации сеток и полученных линий тока
+
+## Входные данные
+Построение происходит на неструктурированных сетках в формате .vtu с определенными векторными значениями Velocity в каждом узле сетки. 
+
+`P.s. Сетки достаточно тяжелые, поэтому если вам нужны исходные данные, вы всегда можете написать мне на почту guvictory@yandex.ru`
+
+## Результат работы программы
+
+### Просто визуализация сетки
+![1](https://github.com/GuVictory/VectorFlowVisualization3D_streamlines/blob/main/img/1.png)
+### Визуализация сетки с построенными линиями тока
+![2](https://github.com/GuVictory/VectorFlowVisualization3D_streamlines/blob/main/img/2.png)
+### Визуализация сетки и сетки с построенными линиями тока
+![4](https://github.com/GuVictory/VectorFlowVisualization3D_streamlines/blob/main/img/4.png)
+### Визуализация одиной сетки в разные моменты времени
+![3](https://github.com/GuVictory/VectorFlowVisualization3D_streamlines/blob/main/img/3.png)
+![5](https://github.com/GuVictory/VectorFlowVisualization3D_streamlines/blob/main/img/5.png)
+### Визуализация объединенной сетки
+![6](https://github.com/GuVictory/VectorFlowVisualization3D_streamlines/blob/main/img/6.png)
--- main.cpp
+++ main.cpp
@@ -45,7 +45,7 @@ int main(int argc, char** argv) {
 
     int meshNumber = 0;
     int renderType = 0;
-    std::cout << "На какой построить линии тока? (0: rotor, 1: eintritt, 2: diffusor)" << std::endl;
+    std::cout << "На какой сетке построить линии тока? (0: rotor, 1: eintritt, 2: diffusor)" << std::endl;
     std::cin >> meshNumber;
     std::cout << "В каком формате построить данные?" << std::endl <<
         "0: только сетка\n"
@@ -425,7 +425,7 @@ int main(int argc, char** argv) {
 
             vtkNew<vtkActor> streamlineActor1;
             streamlineActor1->SetMapper(streamlineMapper1.Get());
-            infoLog("Расчет для построения линий тока закончен");
+            infoLog("Расчет для построения линий тока в первый момент времени закончен закончен");
 
             topRightRenderer->AddActor(streamlineActor1.Get());
 
@@ -529,6 +529,248 @@ int main(int argc, char** argv) {
             widget->renderWindow()->AddRenderer(topRightRenderer);
             widget->renderWindow()->AddRenderer(bottomLeftRenderer);
             widget->renderWindow()->AddRenderer(bottomRightRenderer);
+
+            break;
+        }
+        case 4: {
+            infoLog("Загружаю первую сетку...");
+            // Считаем сетку
+            vtkSmartPointer<vtkXMLUnstructuredGridReader> meshReader1 = vtkSmartPointer<vtkXMLUnstructuredGridReader>::New();
+            meshReader1->SetFileName(meshes.at(0).c_str());
+            meshReader1->Update();
+            infoLog("Первая сетка загружена");
+
+            infoLog("Загружаю вторую сетку...");
+            // Считаем сетку
+            vtkSmartPointer<vtkXMLUnstructuredGridReader> meshReader2 = vtkSmartPointer<vtkXMLUnstructuredGridReader>::New();
+            meshReader2->SetFileName(meshes.at(1).c_str());
+            meshReader2->Update();
+            infoLog("Вторая сетка загружена");
+
+            infoLog("Загружаю третью сетку...");
+            // Считаем сетку
+            vtkSmartPointer<vtkXMLUnstructuredGridReader> meshReader3 = vtkSmartPointer<vtkXMLUnstructuredGridReader>::New();
+            meshReader3->SetFileName(meshes.at(2).c_str());
+            meshReader3->Update();
+            infoLog("Третья сетка загружена");
+
+
+            double leftViewport[4] = {0.0, 0.0, 0.5, 1.0};
+            double rightViewport[4] = {0.5, 0.0, 1.0, 1.0};
+
+
+            // Переменная в которой происходит весь рендер
+            vtkNew<vtkRenderer> leftRenderer;
+            leftRenderer->SetViewport(leftViewport);
+
+            vtkNew<vtkRenderer> rightRenderer;
+            rightRenderer->SetViewport(rightViewport);
+
+
+            /// Отрисуем сначала саму сетку
+            vtkNew<vtkPlane> plane1;
+            plane1->SetOrigin(0.0, 0.0, 0.0);
+            plane1->SetNormal(0.0, 0.0, 0.0);
+            vtkNew<vtkClipDataSet> clipper1;
+            clipper1->SetInputConnection(meshReader1->GetOutputPort());
+            clipper1->SetClipFunction(plane1.Get());
+
+            vtkNew<vtkDataSetMapper> wireMapper1;
+            wireMapper1->SetInputConnection(clipper1->GetOutputPort());
+            wireMapper1->ScalarVisibilityOff();
+
+            vtkNew<vtkActor> wireActor1;
+            wireActor1->SetMapper(wireMapper1.Get());
+            wireActor1->GetProperty()->SetRepresentationToWireframe();
+            wireActor1->GetProperty()->SetColor(0.4, 0.4, 0.4);
+            wireActor1->GetProperty()->SetLineWidth(0.5);
+            wireActor1->GetProperty()->SetOpacity(0.1);
+
+            vtkNew<vtkPlane> plane2;
+            plane2->SetOrigin(0.0, 0.0, 0.0);
+            plane2->SetNormal(0.0, 0.0, 0.0);
+            vtkNew<vtkClipDataSet> clipper2;
+            clipper2->SetInputConnection(meshReader2->GetOutputPort());
+            clipper2->SetClipFunction(plane2.Get());
+
+            vtkNew<vtkDataSetMapper> wireMapper2;
+            wireMapper2->SetInputConnection(clipper2->GetOutputPort());
+            wireMapper2->ScalarVisibilityOff();
+
+            vtkNew<vtkActor> wireActor2;
+            wireActor2->SetMapper(wireMapper2.Get());
+            wireActor2->GetProperty()->SetRepresentationToWireframe();
+            wireActor2->GetProperty()->SetColor(0.4, 0.4, 0.4);
+            wireActor2->GetProperty()->SetLineWidth(0.5);
+            wireActor2->GetProperty()->SetOpacity(0.1);
+
+            vtkNew<vtkPlane> plane3;
+            plane3->SetOrigin(0.0, 0.0, 0.0);
+            plane3->SetNormal(0.0, 0.0, 0.0);
+            vtkNew<vtkClipDataSet> clipper3;
+            clipper3->SetInputConnection(meshReader3->GetOutputPort());
+            clipper3->SetClipFunction(plane3.Get());
+
+            vtkNew<vtkDataSetMapper> wireMapper3;
+            wireMapper3->SetInputConnection(clipper3->GetOutputPort());
+            wireMapper3->ScalarVisibilityOff();
+
+            vtkNew<vtkActor> wireActor3;
+            wireActor3->SetMapper(wireMapper3.Get());
+            wireActor3->GetProperty()->SetRepresentationToWireframe();
+            wireActor3->GetProperty()->SetColor(0.4, 0.4, 0.4);
+            wireActor3->GetProperty()->SetLineWidth(0.5);
+            wireActor3->GetProperty()->SetOpacity(0.1);
+
+            leftRenderer->AddActor(wireActor1.Get());
+            rightRenderer->AddActor(wireActor1.Get());
+
+            leftRenderer->AddActor(wireActor2.Get());
+            rightRenderer->AddActor(wireActor2.Get());
+
+            leftRenderer->AddActor(wireActor3.Get());
+            rightRenderer->AddActor(wireActor3.Get());
+
+
+            /// Отрисуем лини тока
+            infoLog("Выполняю расчет для построения линий тока на первой сетке...");
+            // Сгенерируем стартовые точки для линий тока
+            vtkNew<vtkPointSource> pointSource1;
+            pointSource1->SetCenter(0.0, 0.0, 0.0);
+            pointSource1->SetRadius(0.3);
+            pointSource1->SetDistributionToUniform();
+            pointSource1->SetNumberOfPoints(3000);
+
+            // Установим, что направление расчета происходит как в положительном так и в отрицательном направлении
+            // Установим метод для расчета точек линии тока - Рунге Кутты 4го порядка
+            vtkNew<vtkStreamTracer> tracer1;
+            tracer1->SetInputConnection(clipper1->GetOutputPort());
+            tracer1->SetSourceConnection(pointSource1->GetOutputPort());
+            tracer1->SetIntegrationDirectionToBoth();
+            tracer1->SetIntegratorTypeToRungeKutta45();
+
+            vtkNew<vtkArrayCalculator> magCalc1;
+            magCalc1->SetInputConnection(tracer1->GetOutputPort());
+            magCalc1->AddVectorArrayName("Velocity");
+            magCalc1->SetResultArrayName("MagVelocity");
+            magCalc1->SetFunction("mag(Velocity)");
+
+            magCalc1->Update();
+            double magVelocityRange1[2];
+            magCalc1->GetDataSetOutput()->GetPointData()->GetArray("MagVelocity")->GetRange(magVelocityRange1);
+
+            // Отрисуем не просто линии, а цилендрическии поверхности определенного радиуса
+            vtkNew<vtkRibbonFilter> ribbonFilter1;
+            ribbonFilter1->SetInputConnection(magCalc1->GetOutputPort());
+            ribbonFilter1->SetWidth(0.001);
+
+            vtkNew<vtkPolyDataMapper> streamlineMapper1;
+            streamlineMapper1->SetInputConnection(ribbonFilter1->GetOutputPort());
+            streamlineMapper1->SelectColorArray("MagVelocity");
+            streamlineMapper1->SetScalarRange(magVelocityRange1);
+
+            vtkNew<vtkActor> streamlineActor1;
+            streamlineActor1->SetMapper(streamlineMapper1.Get());
+            infoLog("Расчет для построения линий тока на первой сетке закончен");
+
+            rightRenderer->AddActor(streamlineActor1.Get());
+
+            /// Отрисовка линий тока закончена
+
+            /// Отрисуем лини тока
+            infoLog("Выполняю расчет для построения линий тока на второй сетке...");
+            // Сгенерируем стартовые точки для линий тока
+            vtkNew<vtkPointSource> pointSource2;
+            pointSource2->SetCenter(0.0, 0.0, 0.0);
+            pointSource2->SetRadius(0.3);
+            pointSource2->SetDistributionToUniform();
+            pointSource2->SetNumberOfPoints(3000);
+
+            // Установим, что направление расчета происходит как в положительном так и в отрицательном направлении
+            // Установим метод для расчета точек линии тока - Рунге Кутты 4го порядка
+            vtkNew<vtkStreamTracer> tracer2;
+            tracer2->SetInputConnection(clipper2->GetOutputPort());
+            tracer2->SetSourceConnection(pointSource2->GetOutputPort());
+            tracer2->SetIntegrationDirectionToBoth();
+            tracer2->SetIntegratorTypeToRungeKutta45();
+
+            vtkNew<vtkArrayCalculator> magCalc2;
+            magCalc2->SetInputConnection(tracer2->GetOutputPort());
+            magCalc2->AddVectorArrayName("Velocity");
+            magCalc2->SetResultArrayName("MagVelocity");
+            magCalc2->SetFunction("mag(Velocity)");
+
+            magCalc2->Update();
+            double magVelocityRange2[2];
+            magCalc2->GetDataSetOutput()->GetPointData()->GetArray("MagVelocity")->GetRange(magVelocityRange2);
+
+            // Отрисуем не просто линии, а цилендрическии поверхности определенного радиуса
+            vtkNew<vtkRibbonFilter> ribbonFilter2;
+            ribbonFilter2->SetInputConnection(magCalc2->GetOutputPort());
+            ribbonFilter2->SetWidth(0.001);
+
+            vtkNew<vtkPolyDataMapper> streamlineMapper2;
+            streamlineMapper2->SetInputConnection(ribbonFilter2->GetOutputPort());
+            streamlineMapper2->SelectColorArray("MagVelocity");
+            streamlineMapper2->SetScalarRange(magVelocityRange2);
+
+            vtkNew<vtkActor> streamlineActor2;
+            streamlineActor2->SetMapper(streamlineMapper2.Get());
+            infoLog("Расчет для построения линий тока на второй сетке закончен");
+
+            rightRenderer->AddActor(streamlineActor2.Get());
+
+            /// Отрисовка линий тока закончена
+
+            /// Отрисуем лини тока
+            infoLog("Выполняю расчет для построения линий тока на третьей сетке...");
+            // Сгенерируем стартовые точки для линий тока
+            vtkNew<vtkPointSource> pointSource3;
+            pointSource3->SetCenter(0.0, 0.0, 0.0);
+            pointSource3->SetRadius(0.3);
+            pointSource3->SetDistributionToUniform();
+            pointSource3->SetNumberOfPoints(3000);
+
+            // Установим, что направление расчета происходит как в положительном так и в отрицательном направлении
+            // Установим метод для расчета точек линии тока - Рунге Кутты 4го порядка
+            vtkNew<vtkStreamTracer> tracer3;
+            tracer3->SetInputConnection(clipper3->GetOutputPort());
+            tracer3->SetSourceConnection(pointSource3->GetOutputPort());
+            tracer3->SetIntegrationDirectionToBoth();
+            tracer3->SetIntegratorTypeToRungeKutta45();
+
+            vtkNew<vtkArrayCalculator> magCalc3;
+            magCalc3->SetInputConnection(tracer3->GetOutputPort());
+            magCalc3->AddVectorArrayName("Velocity");
+            magCalc3->SetResultArrayName("MagVelocity");
+            magCalc3->SetFunction("mag(Velocity)");
+
+            magCalc3->Update();
+            double magVelocityRange3[2];
+            magCalc3->GetDataSetOutput()->GetPointData()->GetArray("MagVelocity")->GetRange(magVelocityRange3);
+
+            // Отрисуем не просто линии, а цилендрическии поверхности определенного радиуса
+            vtkNew<vtkRibbonFilter> ribbonFilter3;
+            ribbonFilter3->SetInputConnection(magCalc3->GetOutputPort());
+            ribbonFilter3->SetWidth(0.001);
+
+            vtkNew<vtkPolyDataMapper> streamlineMapper3;
+            streamlineMapper3->SetInputConnection(ribbonFilter3->GetOutputPort());
+            streamlineMapper3->SelectColorArray("MagVelocity");
+            streamlineMapper3->SetScalarRange(magVelocityRange3);
+
+            vtkNew<vtkActor> streamlineActor3;
+            streamlineActor3->SetMapper(streamlineMapper3.Get());
+            infoLog("Расчет для построения линий тока на третьей сетке закончен");
+
+            rightRenderer->AddActor(streamlineActor3.Get());
+
+            /// Отрисовка линий тока закончена
+            leftRenderer->SetBackground(colors->GetColor3d("White").GetData());
+            rightRenderer->SetBackground(colors->GetColor3d("White").GetData());
+
+            widget->renderWindow()->AddRenderer(leftRenderer);
+            widget->renderWindow()->AddRenderer(rightRenderer);
 
             break;
         }
